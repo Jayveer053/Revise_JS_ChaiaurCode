3e25e69299cc250b63a11db6cf851cfc02dfbbb2
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-26 17:09:01 +0000

    Scope of an variable In JavaScript

---
+++ .devcontainer/01_basic/02_Variables.js
@@ -0,0 +1,33 @@
+// alert("Hello");
+
+//We are Using NodeJs Not Browser
+
+console.log(3+
+    3);  // Code Resuseability Should be in Priority
+console.log("Jayveer")
+
+
+let name = "Jayveer";
+let age = 21
+
+let isLoggedIn = false
+
+
+
+//number =>2 to power
+// bigint
+//String => ""
+// boolean => True, False
+//null => Standalone Value
+
+//Undefined =>
+//Symbol => Unique
+
+
+
+
+//object =>
+
+
+console.log(typeof(isLoggedIn));
+
