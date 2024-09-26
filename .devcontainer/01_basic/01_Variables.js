6ee3cec12dd8f39c909ed8ad8c8a84de4b69b0ea
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-26 16:53:39 +0000

    Datatypes in javascript

---
+++ .devcontainer/01_basic/01_Variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "jayveerpal22223.com";
+var accountPassword = "123456";
+
+accountCity = "Bareilly";
+
+// You can not change the value of const variable
+/* 
+accountId = 22;
+console.log(accountId);
+*/
+
+
+/*
+
+prefer not to use var
+Because of issue in block scope and function scope
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity]);
