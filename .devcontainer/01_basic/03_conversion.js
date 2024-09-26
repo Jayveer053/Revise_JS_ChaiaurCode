482329d22d78bc4edc78e38f46c4aa50c9c23ef2
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-26 17:55:02 +0000

    operation on variales

---
+++ .devcontainer/01_basic/03_conversion.js
@@ -0,0 +1,53 @@
+/*let score = "33"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+console.log(typeof (valueInNumber));*/
+
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+
+/*let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+// 1 => truee; 
+// 0 => False
+
+/*
+let someNumber = 33
+
+let stringNumber = String(stringNumber);
+console.log(stringNumber);
+*/
+
+
+// ********************************************* Operation *****************************
+let value = 3
+
+let negValue = -value
+console.log(negValue)
+
+
+
+let str1 = "Hello"
+let str2 = "Jayveer"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log()
