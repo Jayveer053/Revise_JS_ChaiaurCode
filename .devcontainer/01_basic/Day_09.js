c50c67ad0e08932fe1f37cbee5e8b084fd55ec21
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-27 14:06:58 +0000

    Datatype Summary

---
+++ .devcontainer/01_basic/Day_09.js
@@ -0,0 +1,52 @@
+// Primitive
+
+/*
+
+1. String
+2. Number
+3. Boolean
+4. Null
+5. Undefined
+6. Symbol
+7. BigInt
+
+*/
+const Score = 100
+const scoreValue = 1000.3
+
+const isLogged = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bogNumber = 33333442333121233n
+
+
+
+
+// Reference Data Types
+/*
+1. Array
+2. Objects
+3. Functions
+
+*/
+
+
+const heros = ["Shaktiman","Naagraj","Doga"]
+
+let obj ={
+    name:"jayveer",
+    age: "21",
+}
+
+
+const myfunction = function(){
+    console.log("Hello World");
+    
+}
