eccab9eda24ee5508406a3797c6a8a45a07fc368
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-27 16:00:13 +0000

    String

---
+++ .devcontainer/01_basic/Day_11.js
@@ -0,0 +1,15 @@
+const name = "Jayveer"
+const reopCount = 50
+
+console.log(`Hello my name is ${name} and the value pf count is ${reopCount}`);
+
+
+
+
+const gameName = new String("Jayveer Pal");
+// console.log(gameName[0]);
+console.log(gameName.__proto__)
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(7));
+
