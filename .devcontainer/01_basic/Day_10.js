568af0fc66e8a86dbc196574d77471d20fe6b067
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-27 15:49:48 +0000

    dataTypes in Details

---
+++ .devcontainer/01_basic/Day_10.js
@@ -0,0 +1,26 @@
+// Stack (Primittive) , Heap (Non-Primitive)
+
+let myYoutube = "Jayveerpal";
+
+
+
+
+
+let anothername =   myYoutubename
+anothername = "chaiorcode"
+
+
+
+
+
+let user = {
+    email:"jayveerpal333",
+    upi:"jayveer@paytm"
+}
+
+let user2 = user
+
+user2.email = "shivampal2222@gmail.com"
+
+console.log(user);
+console.log(user2);
