a496af0b567b53be9f629a1f2eeefd7681d61d3b
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-28 06:36:23 +0000

    NumberS In JavaScript

---
+++ .devcontainer/01_basic/Day_12.js
@@ -0,0 +1,29 @@
+// const score = 4000
+
+// const balance = new Number(1000)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+// const otherNumber = 23.90900;
+// console.log(otherNumber.toPrecision(3));
+
+
+
+
+// const hundress = 100000000
+// console.log(hundress.toLocaleString('en-IN'))
+
+
+const hundreds = 10000
+console.log(Math);
+console.log(Math.abs(-999));
+
+
+
+const min = 10;
+const max = 20;
+console.log(Math.random() * (max - min + 1)) + min
