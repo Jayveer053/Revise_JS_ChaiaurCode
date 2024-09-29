c556643c17d7b7e63ec8b037b4f84986f8293216
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-29 13:24:30 +0000

    SwitchCase StateMent

---
+++ .devcontainer/04_Control_flow/Day_26_part_2.js
@@ -0,0 +1,36 @@
+/*switch(key){
+    case value:
+
+
+    break
+
+    default
+
+    break
+}
+*/
+
+const month = "March"
+switch(month){
+    case 'Jan':
+        console.log("January");
+        break
+        
+    case 'Feb':
+        console.log("February");
+        break
+     
+    case 'March':
+        console.log("March");
+        break
+     
+    case 'april':
+        console.log("April");
+        break
+    case 'may':
+        console.log("May");
+        break 
+    default:
+        console.log("Default Case Match") 
+        break
+}
