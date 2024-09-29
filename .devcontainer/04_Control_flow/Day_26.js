ffbc48aa70dc84c5ab7a7aa486c1b25d44533b72
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-29 12:57:36 +0000

    ControlFlow IN JS

---
+++ .devcontainer/04_Control_flow/Day_26.js
@@ -0,0 +1,57 @@
+/*
+const isUserLoggedIn = true
+const temprature = 41
+
+
+if (temprature === 40){
+    console.log("Less than 50")
+} else{
+    console.log("Temprature is greater than 50")
+}
+*/
+
+// console.log("Execute");
+// <, >, <=, >=, ==, ===, !==
+
+
+
+/*
+const score = 200
+if (score > 100){
+    let power = "fly"
+    console.log(`User power: ${power}`);
+    
+}
+
+*/
+const balance = 1000
+
+// if (balance > 500) console.log("Test"),console.log("test2");
+
+/*
+if (balance < 500){
+    console.log("Less than 500");
+}else if(balance < 750){
+    console.log("Less than 750")
+}else if(balance < 900){
+    console.log("Less than 900")
+}else{
+    console.log("Less than 1200");
+    
+}
+    */
+
+
+const userLogged = true 
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (userLogged && debitcard && 2==3){
+    console.log("Allow to buy course:")
+}
+
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in")
+}
