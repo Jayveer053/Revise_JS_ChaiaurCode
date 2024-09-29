ae4e925b032e41e1759a7c03c6ef28b0910c225b
Jayveer053 <jayveerpal2222@gmail.com>
2024-09-29 05:43:32 +0000

    Scope In JavaScript

---
+++ .devcontainer/03_basic/Day_21.js
@@ -0,0 +1,41 @@
+//var c = 300
+/*
+
+let a = 300
+if (true){
+    let a =10
+    const b = 20
+    console.log(`INNER, ${a}`)
+}
+
+*/
+
+
+/*
+function one(){
+    const username = "Jayveer"
+
+    function two(){
+        const website = "Instagram";
+        console.log(`The username is ${username}`)
+    }
+
+    // console.log(website)
+    console.log(two())
+}
+one()
+*/
+
+
+
+function addone(num){
+    return num + 1
+
+
+
+const addTwo = function(){
+    return num + 2
+}
+addTwo(5)
+}
+console.log(addone(5))
